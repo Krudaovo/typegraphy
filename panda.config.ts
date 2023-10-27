dd47325077cca27ae30fa6fb10ea876522d356c3
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2023-10-27 12:21:21 +0900

    add breakpoints

--- panda.config.ts
+++ panda.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {
+      breakpoints: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px'
+      },
       tokens: {
         colors: {
           // entry theme
